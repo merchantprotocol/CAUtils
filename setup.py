dbed9fd04b2563452dd08cc87bb39d41382f29b2
Jonathon Byrdziak <jonathonbyrd@gmail.com>
2023-06-19 13:46:21 -0700

    initial

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='CAUtils',
+    version='0.1',
+    packages=find_packages(),
+    description='A utility library for CA',
+    author='Your Name',
+    author_email='jonathon@capitalaudience.com',
+    url='https://github.com/merchantprotocol/CAUtils',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+    keywords='utility, CA, example, tutorial',
+    project_urls={
+        'Bug Reports': 'https://github.com/merchantprotocol/CAUtils/issues',
+        'Source': 'https://github.com/merchantprotocol/CAUtils',
+    },
+)
